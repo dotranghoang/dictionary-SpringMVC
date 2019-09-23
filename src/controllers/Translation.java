9d578bd98cb96fa08ff4c8c3ff4e3f9d55bfa190
dotranghoang <dotranghoang@gmail.com>
2019-09-23 18:23:47 +0700

    Dictionary SpringMVC

---
+++ src/controllers/Translation.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Translation {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/translate")
+    public String translate(@RequestParam String english , ModelMap modelMap ) {
+        english = english.toLowerCase();
+        modelMap.put("book","Quyển sách");
+        modelMap.put("goodbye","Tạm biệt");
+        modelMap.put("hello","Xin chào");
+        modelMap.put("apple","Quả táo");
+
+       String result = (String) modelMap.get(english);
+       if(result != null) {
+           modelMap.addAttribute("english",english);
+           modelMap.addAttribute("result",result);
+       } else {
+           result = "Not found !";
+           modelMap.addAttribute("english",english);
+           modelMap.addAttribute("result",result);
+       }
+       return "/index";
+    }
+
+}
